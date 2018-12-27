ecfd01c643fe33a35a44be1fa54045acfdfdcead
hengyi <2937638192@qq.com>
2018-12-27 14:26:26 +0800

    后台消息通知 链接。

--- js/main.js
+++ js/main.js
@@ -119,6 +119,22 @@ function demo2(url, fn, curr = 1, limit, data = "") {
 	});
 
 }
+(function($) {
 
+	var o = $({});
+  
+	$.subscribe = function() {
+	  o.on.apply(o, arguments);
+	};
+  
+	$.unsubscribe = function() {
+	  o.off.apply(o, arguments);
+	};
+  
+	$.publish = function() {
+	  o.trigger.apply(o, arguments);
+	};
+  
+  }(jQuery));
 
 
